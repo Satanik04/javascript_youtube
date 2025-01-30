92effb51af876365a7222a85ad98fbaba233fd1b
Satanik Manna <mannasatanik8@gmail.com>
2025-01-30 17:48:43 +0000

    date

---
+++ one_first/date.js
@@ -0,0 +1,9 @@
+let mydate=new Date()
+console.log(mydate)
+
+console.log(toString().mydate)
+console.log(mydate.toISOString())
+console.log(mydate.toDateString())
+
+let mycreateddate=new Date(2025,0,1)
+console.log(mycreateddate.toDateString())
