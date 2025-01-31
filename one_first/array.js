eb16ddcd11b118ccbe716d99709601dd2034b393
Satanik Manna <mannasatanik8@gmail.com>
2025-01-31 03:06:24 +0000

    array

---
+++ one_first/array.js
@@ -0,0 +1,15 @@
+// const arr=[1,2,3,4]
+// console.log(arr[0]);
+// console.log(arr.push(8,9))
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// console.log(arr.indexOf(3))
+
+const myarr=[0,1,2,3,4]
+const myn1=myarr.slice(1,3)
+console.log(myn1)
+
+const myn2=myarr.splice(1,3)
+console.log(myarr)
+console.log(myn2) 
