fd7ce0cd049635381bab6e7fc2fc1bbcdc7f549c
Satanik Manna <mannasatanik8@gmail.com>
2025-02-02 05:22:00 +0000

    function

--- one_first/function.js
+++ one_first/function.js
@@ -16,3 +16,23 @@ function addnumber(num1,num2){
 }
 const result=addnumber(4,5)
 console.log("result:",result)
+
+function loginuser(username){
+    return `${username} just logged in `
+}
+console.log(loginuser("satanik"))
+
+//shopping card add elements
+function addcardelement(...num){
+    return num
+}
+console.log(addcardelement(122,342,223,224))
+
+const user={    //object
+   username:"satanik",
+   price:199  
+}
+function handleobject(anyobj){
+   console.log(`username is ${anyobj.username} and price is ${anyobj.price}`)
+}
+handleobject(user)
