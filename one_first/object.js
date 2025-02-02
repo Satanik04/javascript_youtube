3d31d947397049d11ed2318e5d40b36fbe156348
Satanik Manna <mannasatanik8@gmail.com>
2025-02-02 03:39:43 +0000

    object

--- one_first/object.js
+++ one_first/object.js
@@ -17,6 +17,9 @@ const tinderuser={}
 tinderuser.id="123abc"
 tinderuser.name="satanik"
 console.log(tinderuser)
+console.log(Object.keys(tinderuser))  //convert an array of an element
+console.log(Object.values(tinderuser)) 
+
 
 const regularuser={  //nested object
     email:"satanik@gmail.com",
@@ -29,3 +32,26 @@ const regularuser={  //nested object
 }
 
 console.log(regularuser.fullname.userfullname)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+const obj3={5:"e",6:"f"}
+
+// const obj4=Object.assign({},obj1,obj2,obj3)
+// console.log(obj4)
+
+const obj4={...obj1,...obj2} //spread operation
+console.log(obj4)
+
+const obj6=[
+    {id:2344,email:"mannasatanik8@gmail.com"},
+    {id:234484,email:"mannasatanik6@gmail.com"}
+] //array of an object
+console.log(obj6[1].id) //access
+
+const info={
+    courseinstructure:"hitesh",
+    age:30
+}
+const{courseinstructure:instructure}=info
+console.log(instructure)
